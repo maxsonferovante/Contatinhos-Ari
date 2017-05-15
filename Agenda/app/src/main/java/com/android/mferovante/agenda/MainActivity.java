fa74ab52ccef13bbbee8ebe53621665492531200
Maxson Ferovante <maxsonferovante@gmail.com>
2017-05-15 15:57:26 -0300

    RecyclearListView dos Canditatos implementado em um fragment [PagerView-Matricualdo]

--- Agenda/app/src/main/java/com/android/mferovante/agenda/MainActivity.java
+++ Agenda/app/src/main/java/com/android/mferovante/agenda/MainActivity.java
@@ -1,52 +1,31 @@
 package com.android.mferovante.agenda;
 
-import android.Manifest;
-import android.app.Instrumentation;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.util.LruCache;
-import android.support.v7.app.AlertDialog;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.PopupMenu;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.ContextMenu;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Toast;
 
-import com.android.mferovante.agenda.adapter.ContatosAdapter;
-import com.android.mferovante.agenda.database.ContatoDatabase;
-import com.android.mferovante.agenda.modelo.Contato;
+import com.android.mferovante.agenda.fragments.CanditatosFragment;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.locks.AbstractQueuedLongSynchronizer;
-
-import me.drakeet.materialdialog.MaterialDialog;
 
 public class MainActivity extends AppCompatActivity{
-    private Contato aux = null;
-
-    private static final int REQUEST_PERMISSIONS_CODE = 128;
-    private static final String TAG = "LOG";
-    private List<Contato> contatoList = new ArrayList<Contato>();
-    private RecyclerView recyclerView;
-    private ContatosAdapter contatosAdapter;
-    private ContatoDatabase contatoDatabase;
+    public static final int REQUEST_PERMISSIONS_CODE = 128;
+    public static final String TAG = "LOG";
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private ViewPagerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,9 +33,16 @@ public class MainActivity extends AppCompatActivity{
 
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
 
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -71,162 +57,15 @@ public class MainActivity extends AppCompatActivity{
     @Override
     protected void onResume() {
         super.onResume();
-
-        setRecyclerView();
-
-        recyclerView.addOnItemTouchListener(new RecyclearTouchListener(getApplicationContext(),
-                recyclerView,
-                new RecyclearTouchListener.ClickListener() {
-                    @Override
-                    public void onClick(View view, final int position) {
-                        final Contato contato = contatoList.get(position);
-                        //Lista de itens
-                        ArrayList<String> itens = new ArrayList<String>();
-                        itens.add("Efetuar ligação");
-                        itens.add("Editação dos dados");
-                        itens.add("Excluir contato");
-                        itens.add("Matriculado");
-
-                        //adapter utilizando um layout customizado (TextView)
-                        ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, R.layout.support_simple_spinner_dropdown_item, itens);
-
-                        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                        builder.setTitle("Opções");
-
-                        //define o diálogo como uma lista, passa o adapter.
-                        builder.setSingleChoiceItems(adapter, 0, new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface arg0, int arg1) {
-                                arg0.dismiss();
-                                onMenuItemClickAlertDialog(arg1, contato, position);
-                            }
-                        });
-
-                        builder.create().show();
-                    }
-
-                    @Override
-                    public void onLongClick(View view, int position) {
-                        Contato contato = contatoList.get(position);
-                        Toast.makeText(getApplicationContext(),
-                                "Novas funcionabilidades serão entreges futuramente!",
-                                Toast.LENGTH_SHORT).show();
-                    }
-                }));
     }
 
-    private void setRecyclerView() {
-        contatoDatabase = new ContatoDatabase(this);
-
-        contatoList = contatoDatabase.buscaContatos();
-
-        contatosAdapter = new ContatosAdapter(contatoList);
-
-        recyclerView = (RecyclerView) findViewById(R.id.recycle_list_view);
-
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
-
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.setAdapter(contatosAdapter);
+    private void setupViewPager(ViewPager viewPager) {
+        adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new CanditatosFragment(), "Canditatos");
+        viewPager.setAdapter(adapter);
     }
 
-    public void onMenuItemClickAlertDialog(int itemSelected, final Contato contato, final int positionContact) {
-        switch (itemSelected) {
-            case 0:
-                aux = contato;
-                callAccessPhone();
-            break;
-            case 1:
-                Intent intent = new Intent(MainActivity.this, ContatoActivity.class);
-                intent.putExtra("contato", contato);
-                startActivity(intent);
-                break;
-            case 2:
-                new AlertDialog.Builder(this)
-                        .setMessage(R.string.deseja_apagar)
-                        .setCancelable(false)
-                        .setPositiveButton(R.string.sim, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                contatoDatabase = new ContatoDatabase(getApplicationContext());
-                                contatoDatabase.deletar(contato);
-                                setRecyclerView();
-                            }
-                        })
-                        .setNegativeButton(R.string.nao, null).show();
-                break;
-            case 3:
-                Toast.makeText(this, "Matriculado Clicked", Toast.LENGTH_SHORT).show();
-                break;
-        }
 
-    }
-    private  void callAccessPhone(){
-        if( ContextCompat.checkSelfPermission( this, Manifest.permission.CALL_PHONE ) != PackageManager.PERMISSION_GRANTED ){
-
-            if( ActivityCompat.shouldShowRequestPermissionRationale( this, Manifest.permission.CALL_PHONE ) ){
-                callDialog( "Precisamos do acesso ao Telefone para efetuares contato com os canditatos.", new String[]{Manifest.permission.CALL_PHONE} );
-            }
-            else{
-                ActivityCompat.requestPermissions( this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_PERMISSIONS_CODE );
-            }
-        }
-        else{
-            callPhone();
-        }
-    }
-    private void callDialog( String message, final String[] permissions ){
-        final MaterialDialog mMaterialDialog = new MaterialDialog(this);
-
-        mMaterialDialog.setTitle("Permission");
-        mMaterialDialog.setMessage(message);
-
-        mMaterialDialog.setPositiveButton("Ok", new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-
-                        ActivityCompat.requestPermissions(MainActivity.this, permissions, REQUEST_PERMISSIONS_CODE);
-                        mMaterialDialog.dismiss();
-                    }
-                })
-                .setNegativeButton("Cancel", new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        mMaterialDialog.dismiss();
-                    }
-                });
-        mMaterialDialog.show();
-    }
-    private void callPhone(){
-        Intent call = new Intent(Intent.ACTION_DIAL,
-                Uri.parse("tel:" + aux.getTelefone()
-                )
-        );
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)
-                == PackageManager.PERMISSION_GRANTED) {
-            startActivity(call);
-        } else {
-            Log.i("PERMISSION", "FAIL");
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        Log.i(TAG, "test");
-        switch( requestCode ){
-            case REQUEST_PERMISSIONS_CODE:
-                for( int i = 0; i < permissions.length; i++ ){
-
-                    if( permissions[i].equalsIgnoreCase( Manifest.permission.CALL_PHONE)
-                            && grantResults[i] == PackageManager.PERMISSION_GRANTED ){
-
-                        callPhone();
-                    }
-                }
-        }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -256,5 +95,30 @@ public class MainActivity extends AppCompatActivity{
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
     }
 
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
 
 }
